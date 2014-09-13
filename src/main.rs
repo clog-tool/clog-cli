12a85460a3149a9dea6510e9ee9bb648960be217
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-09-14 00:30:13 +0200

    feat(main): parse --repository option from CLI

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,34 @@
 extern crate regex;
 #[phase(plugin)]
 extern crate regex_macros;
+extern crate serialize;
+#[phase(plugin)] extern crate docopt_macros;
+extern crate docopt;
 
 use git::{ LogReaderConfig, get_commits };
 use log_writer::{ LogWriter, LogWriterOptions };
 use section_builder::build_sections;
 use std::io::{File, Open, Write};
+use docopt::FlagParser;
 
 mod common;
 mod git;
 mod log_writer;
 mod section_builder;
 
+docopt!(Args, "clog
+
+               Usage:
+                 clog [--repository=<link>]
+
+               Options:
+                  -h --help     Show this screen.
+                  --version     Show version
+                  --repository=<link>  e.g https://github.com/thoughtram/clog")
+
 fn main () {
+
+    let args: Args = FlagParser::parse().unwrap_or_else(|e| e.exit());
 
     let log_reader_config = LogReaderConfig {
         grep: "^feat|^fix|BREAKING'".to_string(),
@@ -30,7 +46,7 @@ fn main () {
     let sections = build_sections(commits.clone());
     let mut file = File::open_mode(&Path::new("changelog.md"), Open, Write).ok().unwrap();
     let mut writer = LogWriter::new(&mut file, LogWriterOptions { 
-        repository_link: "https://github.com/ajoslin/conventional-changelog".to_string() 
+        repository_link: args.flag_repository 
     });
 
     writer.write_section("Bug Fixes", &sections.fixes);
