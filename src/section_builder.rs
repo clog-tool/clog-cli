88ccacd5bd559e8af996f3e67a5d58fe31b3f87c
Benny Klotz <r3qnbenni@gmail.com>
2014-10-23 12:24:21 +0200

    fix(section_builder): find_or_insert was deprecated, only some verbose workarounds available yet

--- src/section_builder.rs
+++ src/section_builder.rs
@@ -1,4 +1,5 @@
 use std::collections::hashmap::HashMap;
+use std::collections::hashmap;
 use common::{ LogEntry, SectionMap, Feature, Fix };
 
 pub fn build_sections(log_entries: Vec<LogEntry>) -> SectionMap {
@@ -11,14 +12,29 @@ pub fn build_sections(log_entries: Vec<LogEntry>) -> SectionMap {
     for entry in log_entries.into_iter() {
         match entry.commit_type {
             Feature => {
-                sections.features
+                let feature = match sections.features.entry(entry.component.clone()) {
+                    hashmap::Vacant(v) => v.set(Vec::new()),
+                    hashmap::Occupied(o) => o.into_mut()
+                };
+
+                feature.push(entry.clone());
+
+                // see https://github.com/rust-lang/rfcs/issues/353
+                /* sections.features
                         .find_or_insert(entry.component.clone(), Vec::new())
-                        .push(entry.clone());
+                        .push(entry.clone());*/
             },
             Fix => {
-                sections.fixes
+                let fix = match sections.fixes.entry(entry.component.clone()) {
+                    hashmap::Vacant(v) => v.set(Vec::new()),
+                    hashmap::Occupied(o) => o.into_mut()
+                };
+
+                fix.push(entry.clone());
+
+                /* sections.fixes
                         .find_or_insert(entry.component.clone(), Vec::new())
-                        .push(entry.clone());
+                        .push(entry.clone());*/
             },
             _   => {}
         }
